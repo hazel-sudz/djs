0b701ec6bdc13929de13e49457b242431503043c
Hazel Sudzilouski <hazel@sudz.dev>
2025-10-26 15:51:05 -0400

    refactor

--- animation_v2/main.R
+++ animation_v2/main.R
@@ -1,26 +1,4 @@
-
-# Required packages
-packages <- c(
-  "ggspatial",
-  "ggplot2",
-  "gganimate",
-  "dplyr",
-  "lubridate",
-  "viridis",
-  "scales",
-  "ggnewscale"
-)
-
-# Install any packages that are missing
-installed <- installed.packages()[, "Package"]
-for (p in packages) {
-  if (!(p %in% installed)) {
-    install.packages(p, repos = "https://cloud.r-project.org")
-  }
-}
-
-# Load all packages
-lapply(packages, library, character.only = TRUE)
+source ("src/setup.R")
 
 # Define sensor coordinates (lat, lon)
 sensor_coords <- data.frame(
---
+++ animation_v2/src/setup.R
@@ -0,0 +1,29 @@
+import_packages <- function(packages) {
+    # Install any packages that are missing
+    installed <- installed.packages()[, "Package"]
+    for (p in packages) {
+    if (!(p %in% installed)) {
+        install.packages(p, repos = "https://cloud.r-project.org")
+    }
+    }
+
+    # Load all packages
+    lapply(packages, library, character.only = TRUE)
+}
+
+# Required packages
+packages <- c(
+  "ggspatial",
+  "ggplot2",
+  "gganimate",
+  "dplyr",
+  "lubridate",
+  "viridis",
+  "scales",
+  "ggnewscale"
+)
+
+
+
+import_packages(packages=packages)
+
